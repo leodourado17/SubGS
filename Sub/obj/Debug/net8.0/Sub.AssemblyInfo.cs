518e1c0cbce7404215f2fe55b96b20f18e919da4
leodourado17 <leodourado172003@gmail.com>
2024-11-29 13:57:35 -0300

    atualizado

--- Sub/obj/Debug/net8.0/Sub.AssemblyInfo.cs
+++ Sub/obj/Debug/net8.0/Sub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71646d90a4f86434ffd927dd2bbfac0a9ff969a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
